44d336c17b546149f0ffb702804cb8dfe5fe1923
krunalkanojiya12 <k_krunal@lainnovatorslab.com>
2022-06-21 15:47:26 +0530

    Proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,14 +4,15 @@ module.exports = function (app) {
   app.use(
     "^/abc/",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "https://proxy-react-back.herokuapp.com",
+      // target: "http://localhost:8080",
       changeOrigin: true,
       pathRewrite: {
         "^/abc/posdfir@3!!": "/api/login", // rewrite path
         "^/abc/ks!djf!!ksdf": "/api/users", // rewrite path
         "^/abc/uidij!23&dd": "/api/create", // rewrite path
-        "^/abc/ji@213sd@": "/api/logout", // rewrite path
-      },
+        "^/abc/ji@213sd@": "/api/logout" // rewrite path
+      }
     })
   );
 };
